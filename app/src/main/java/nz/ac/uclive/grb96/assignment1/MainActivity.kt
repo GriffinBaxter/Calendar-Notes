39bc53866206be0fd8cdb2951cf65ca2e9d2f106
Griffin Baxter <grb96@uclive.ac.nz>
2022-08-25 22:17:24 +1200

    Added shortcuts (second API feature).

--- app/src/main/java/nz/ac/uclive/grb96/assignment1/MainActivity.kt
+++ app/src/main/java/nz/ac/uclive/grb96/assignment1/MainActivity.kt
@@ -1,11 +1,23 @@
 package nz.ac.uclive.grb96.assignment1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if ("${applicationContext.packageName}.shortcuts.calendar" == intent.action) {
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+            val navController = navHostFragment?.findNavController()
+            navController?.navigate(R.id.action_global_calendar)
+        } else if ("${applicationContext.packageName}.shortcuts.notes" == intent.action) {
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+            val navController = navHostFragment?.findNavController()
+            navController?.navigate(R.id.action_global_notes)
+        }
     }
 }
